123c056d9af21cddb883b98a6ff97f5563be16cb
victormanuelzap <ucerfito@gmail.com>
2025-10-28 21:49:19 -0500

    chore: .gitignore actualizado (VSCode/Windows/stub)

--- astro.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  site: "https://victormanuelzap.github.io/victoranuncios-web/",
-  base: "/victoranuncios-web/",
-  outDir: "dist", // 👈 muy importante: dist, no docs
-  integrations: [tailwind({ applyBaseStyles: false })],
-});
