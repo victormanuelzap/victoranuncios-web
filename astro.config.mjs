f59ad2254a7afcf78e433f76fad7408ae4d3b331
victormanuelzap <ucerfito@gmail.com>
2025-10-29 00:43:23 -0500

    deploy: sitio Victor Anuncios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
+// astro.config.mjs
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://victormanuelzap.github.io/victoranuncios-web/",
-  base: "/victoranuncios-web/",
-  outDir: "docs",
+  site: "https://victormanuelzap.github.io", // dominio base (sin subcarpeta)
+  base: "/victoranuncios-web/",              // nombre del repo
+  outDir: "dist",                            // carpeta de salida para Pages
   integrations: [tailwind({ applyBaseStyles: false })],
 });
