3fcea2dbb0b390e3102382e86c769bc8a810ffe6
victormanuelzap <133459854+victormanuelzap@users.noreply.github.com>
2025-11-21 15:59:13 -0500

    Update site and base configuration in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://victormanuelzap.github.io", // dominio base (sin subcarpeta)
-  base: "/victoranuncios-web/",              // nombre del repo
-  outDir: "dist",                            // carpeta de salida para Pages
+  site: "https://www.victoranuncios.com", // dominio nuevo
+  base: "/",                              // sin subcarpeta
+  outDir: "dist",                         // carpeta de salida para Pages
   integrations: [tailwind({ applyBaseStyles: false })],
 });
